cbc955ef1c13eb85e7beed876565ed6a117f12f0
R-Studios <R-Studios@gmx.de>
2023-01-15 19:57:13 +0100

    - Fixed formatting - SEO improvements - Updated avatar

--- Super-Amazing-Unicorn-Adventure-0000013.js
+++ Super-Amazing-Unicorn-Adventure-0000013.js
@@ -486,28 +486,29 @@ var mapArray = [
   [0, 0, 0, 0, 0],
   [x],
   [0, 0, 0, 0, 0],
-  [x]
+  [x],
 ];
 var is_playing = false;
 init();
 
 function init() {
-
-  main_canvas = document.getElementById('main_canvas');
-  main_ctx = main_canvas.getContext('2d');
+  main_canvas = document.getElementById("main_canvas");
+  main_ctx = main_canvas.getContext("2d");
 
   document.addEventListener("keydown", key_down, false);
   document.addEventListener("keyup", key_up, false);
 
-  requestaframe = (function() {
-      return window.requestAnimationFrame ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.oRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function(callback) {
-              window.setTimeout(callback, 1000 / 60)
-          };
+  requestaframe = (function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      }
+    );
   })();
 
   player = new Player();
@@ -516,59 +517,69 @@ function init() {
 
 function load_media() {
   Spieler1 = new Image();
-  Spieler1.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn_1.png';
+  Spieler1.src = "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn_1.png";
 
   Spieler2 = new Image();
-  Spieler2.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn_2.png';
+  Spieler2.src = "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn_2.png";
 
   Spieler3 = new Image();
-  Spieler3.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn_3.png';
+  Spieler3.src = "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn_3.png";
 
   Spieler2_1 = new Image();
-  Spieler2_1.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn2_1.png';
+  Spieler2_1.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn2_1.png";
 
   Spieler2_2 = new Image();
-  Spieler2_2.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn2_2.png';
+  Spieler2_2.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn2_2.png";
 
   Spieler2_3 = new Image();
-  Spieler2_3.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn2_3.png';
+  Spieler2_3.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn2_3.png";
 
   Spieler3_1 = new Image();
-  Spieler3_1.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn3_1.png';
+  Spieler3_1.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn3_1.png";
 
   Spieler3_2 = new Image();
-  Spieler3_2.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn3_2.png';
+  Spieler3_2.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn3_2.png";
 
   Spieler3_3 = new Image();
-  Spieler3_3.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn3_3.png';
+  Spieler3_3.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn3_3.png";
 
   Spieler4_1 = new Image();
-  Spieler4_1.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn4_1.png';
+  Spieler4_1.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn4_1.png";
 
   Spieler4_2 = new Image();
-  Spieler4_2.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn4_2.png';
+  Spieler4_2.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn4_2.png";
 
   Spieler4_3 = new Image();
-  Spieler4_3.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/einhorn4_3.png';
+  Spieler4_3.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/einhorn4_3.png";
 
   Block = new Image();
-  Block.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/regenbogen.png';
+  Block.src = "images/Super-Amazing-Unicorn-Adventure-0000013/regenbogen.png";
 
   Hintergrund = new Image();
-  Hintergrund.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/himmel.png';
+  Hintergrund.src = "images/Super-Amazing-Unicorn-Adventure-0000013/himmel.png";
 
   Score = new Image();
-  Score.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/score.png';
+  Score.src = "images/Super-Amazing-Unicorn-Adventure-0000013/score.png";
 
   CharakterAuswahl = new Image();
-  CharakterAuswahl.src = 'images/Super-Amazing-Unicorn-Adventure-0000013/charakter_auswahl.png';
+  CharakterAuswahl.src =
+    "images/Super-Amazing-Unicorn-Adventure-0000013/charakter_auswahl.png";
 }
 
 function mouse(e) {
-  var x = e.pageX - document.getElementById('game_object').offsetLeft;
-  var y = e.pageY - document.getElementById('game_object').offsetTop;
-  document.getElementById('x').innerHTML = x;
-  document.getElementById('y').innerHTML = y;
+  var x = e.pageX - document.getElementById("game_object").offsetLeft;
+  var y = e.pageY - document.getElementById("game_object").offsetTop;
+  document.getElementById("x").innerHTML = x;
+  document.getElementById("y").innerHTML = y;
 }
 
 function abrunden(zahl) {
@@ -590,144 +601,218 @@ function Player() {
   this.Charaktergewählt = false;
 }
 
-Player.prototype.draw = function() {
-
+Player.prototype.draw = function () {
   if (this.Charaktergewählt == false) {
-      main_ctx.drawImage(CharakterAuswahl, 0, 0);
-      this.is_leftkey = false;
-      this.is_rightkey = false;
+    main_ctx.drawImage(CharakterAuswahl, 0, 0);
+    this.is_leftkey = false;
+    this.is_rightkey = false;
   } else {
+    main_ctx.drawImage(Hintergrund, 0, 0);
 
-      main_ctx.drawImage(Hintergrund, 0, 0);
-
-      this.check_keys();
-      if (this.animation == 1) {
-          if (this.aktSpieler == 1) {
-              main_ctx.drawImage(Spieler1, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 2) {
-              main_ctx.drawImage(Spieler2_1, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 3) {
-              main_ctx.drawImage(Spieler3_1, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 4) {
-              main_ctx.drawImage(Spieler4_1, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          }
-      } else if (this.animation == 2) {
-          if (this.aktSpieler == 1) {
-              main_ctx.drawImage(Spieler2, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 2) {
-              main_ctx.drawImage(Spieler2_2, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 3) {
-              main_ctx.drawImage(Spieler3_2, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 4) {
-              main_ctx.drawImage(Spieler4_2, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          }
-      } else if (this.animation == 3) {
-          if (this.aktSpieler == 1) {
-              main_ctx.drawImage(Spieler3, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 2) {
-              main_ctx.drawImage(Spieler2_3, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 3) {
-              main_ctx.drawImage(Spieler3_3, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          } else if (this.aktSpieler == 4) {
-              main_ctx.drawImage(Spieler4_3, this.drawX, this.drawY - abrunden(this.scroll_Y));
-          }
+    this.check_keys();
+    if (this.animation == 1) {
+      if (this.aktSpieler == 1) {
+        main_ctx.drawImage(
+          Spieler1,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 2) {
+        main_ctx.drawImage(
+          Spieler2_1,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 3) {
+        main_ctx.drawImage(
+          Spieler3_1,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 4) {
+        main_ctx.drawImage(
+          Spieler4_1,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
       }
+    } else if (this.animation == 2) {
+      if (this.aktSpieler == 1) {
+        main_ctx.drawImage(
+          Spieler2,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 2) {
+        main_ctx.drawImage(
+          Spieler2_2,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 3) {
+        main_ctx.drawImage(
+          Spieler3_2,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 4) {
+        main_ctx.drawImage(
+          Spieler4_2,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      }
+    } else if (this.animation == 3) {
+      if (this.aktSpieler == 1) {
+        main_ctx.drawImage(
+          Spieler3,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 2) {
+        main_ctx.drawImage(
+          Spieler2_3,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 3) {
+        main_ctx.drawImage(
+          Spieler3_3,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      } else if (this.aktSpieler == 4) {
+        main_ctx.drawImage(
+          Spieler4_3,
+          this.drawX,
+          this.drawY - abrunden(this.scroll_Y)
+        );
+      }
+    }
 
-      var posX = 0;
-      var posY = 0;
+    var posX = 0;
+    var posY = 0;
 
-      for (var i = 0; i < mapArray.length; i++) {
-          for (var j = 0; j < mapArray[i].length; j++) {
-              if (mapArray[i][j] == 1) {
-                  main_ctx.drawImage(Block, posX, posY - abrunden(this.scroll_Y), 160, 120);
-              }
-              if (mapArray[i][j] == x) {
-                  this.z = Math.round(Math.random() * 5);
+    for (var i = 0; i < mapArray.length; i++) {
+      for (var j = 0; j < mapArray[i].length; j++) {
+        if (mapArray[i][j] == 1) {
+          main_ctx.drawImage(
+            Block,
+            posX,
+            posY - abrunden(this.scroll_Y),
+            160,
+            120
+          );
+        }
+        if (mapArray[i][j] == x) {
+          this.z = Math.round(Math.random() * 5);
 
-                  if (this.z == 1) {
-                      mapArray[this.wert] = [0, 1, 1, 1, 1];
-                      this.wert += 2;
-                  }
-
-                  if (this.z == 2) {
-                      mapArray[this.wert] = [1, 0, 1, 1, 1];
-                      this.wert += 2;
-                  }
-
-                  if (this.z == 3) {
-                      mapArray[this.wert] = [1, 1, 0, 1, 1];
-                      this.wert += 2;
-                  }
-
-                  if (this.z == 4) {
-                      mapArray[this.wert] = [1, 1, 1, 0, 1];
-                      this.wert += 2;
-                  }
-
-                  if (this.z == 5) {
-                      mapArray[this.wert] = [1, 1, 1, 1, 0];
-                      this.wert += 2;
-                  }
-              }
-              posX += 160;
+          if (this.z == 1) {
+            mapArray[this.wert] = [0, 1, 1, 1, 1];
+            this.wert += 2;
           }
-          posX = 0;
-          posY += 120;
-      }
 
-      if (mapArray[abrunden((this.drawY + 121) / 120)][abrunden((this.drawX + 155) / 160)] == 0 && mapArray[abrunden((this.drawY + 121) / 120)][abrunden((this.drawX) / 160)] == 0) {
-          this.drawY += 5;
-      }
-
-      if ((this.scroll_Y) <= (this.drawY + 120)) {
-          if (this.drawY > 360) {
-              this.scroll_Y += this.speed;
+          if (this.z == 2) {
+            mapArray[this.wert] = [1, 0, 1, 1, 1];
+            this.wert += 2;
           }
-          this.speed = Math.min(2.3, 2 + (this.scroll_Y / 10000));
-      } else {
-          main_ctx.drawImage(Score, 0, 0);
-          main_ctx.fillStyle = "black";
-          main_ctx.font = "50px Arial";
-          main_ctx.fillText("" + this.drawY, 350, 350);
-      }
 
+          if (this.z == 3) {
+            mapArray[this.wert] = [1, 1, 0, 1, 1];
+            this.wert += 2;
+          }
+
+          if (this.z == 4) {
+            mapArray[this.wert] = [1, 1, 1, 0, 1];
+            this.wert += 2;
+          }
+
+          if (this.z == 5) {
+            mapArray[this.wert] = [1, 1, 1, 1, 0];
+            this.wert += 2;
+          }
+        }
+        posX += 160;
+      }
+      posX = 0;
+      posY += 120;
+    }
+
+    if (
+      mapArray[abrunden((this.drawY + 121) / 120)][
+        abrunden((this.drawX + 155) / 160)
+      ] == 0 &&
+      mapArray[abrunden((this.drawY + 121) / 120)][
+        abrunden(this.drawX / 160)
+      ] == 0
+    ) {
+      this.drawY += 5;
+    }
+
+    if (this.scroll_Y <= this.drawY + 120) {
+      if (this.drawY > 360) {
+        this.scroll_Y += this.speed;
+      }
+      this.speed = Math.min(2.3, 2 + this.scroll_Y / 10000);
+    } else {
+      main_ctx.drawImage(Score, 0, 0);
+      main_ctx.fillStyle = "black";
+      main_ctx.font = "50px Arial";
+      main_ctx.fillText("" + this.drawY, 350, 350);
+    }
   }
 };
 
-Player.prototype.check_keys = function() {
-  if (this.is_rightkey == true && this.drawX <= 640 && mapArray[abrunden((this.drawY + 1) / 120)][abrunden((this.drawX + 160) / 160)] == 0 && mapArray[abrunden((this.drawY + 119) / 120)][abrunden((this.drawX + 160) / 160)] == 0) {
-      this.drawX += 5;
-      if (this.animation < 3) {
-          if (this.wait > 0) {
-              this.wait--;
-          } else {
-              this.animation++;
-              this.wait = 5;
-          }
+Player.prototype.check_keys = function () {
+  if (
+    this.is_rightkey == true &&
+    this.drawX <= 640 &&
+    mapArray[abrunden((this.drawY + 1) / 120)][
+      abrunden((this.drawX + 160) / 160)
+    ] == 0 &&
+    mapArray[abrunden((this.drawY + 119) / 120)][
+      abrunden((this.drawX + 160) / 160)
+    ] == 0
+  ) {
+    this.drawX += 5;
+    if (this.animation < 3) {
+      if (this.wait > 0) {
+        this.wait--;
       } else {
-          this.animation = 1;
+        this.animation++;
+        this.wait = 5;
       }
-  } else if (this.is_leftkey == true && this.drawX > 0 && mapArray[abrunden((this.drawY + 1) / 120)][abrunden((this.drawX - 5) / 160)] == 0 && mapArray[abrunden((this.drawY + 119) / 120)][abrunden((this.drawX - 5) / 160)] == 0) {
-      this.drawX -= 5;
-      if (this.animation < 3) {
-          if (this.wait > 0) {
-              this.wait--;
-          } else {
-              this.animation++;
-              this.wait = 5;
-          }
+    } else {
+      this.animation = 1;
+    }
+  } else if (
+    this.is_leftkey == true &&
+    this.drawX > 0 &&
+    mapArray[abrunden((this.drawY + 1) / 120)][
+      abrunden((this.drawX - 5) / 160)
+    ] == 0 &&
+    mapArray[abrunden((this.drawY + 119) / 120)][
+      abrunden((this.drawX - 5) / 160)
+    ] == 0
+  ) {
+    this.drawX -= 5;
+    if (this.animation < 3) {
+      if (this.wait > 0) {
+        this.wait--;
       } else {
-          this.animation = 1;
+        this.animation++;
+        this.wait = 5;
       }
+    } else {
+      this.animation = 1;
+    }
   }
-
-}
+};
 
 function loop() {
   main_ctx.clearRect(0, 0, 800, 600);
   player.draw();
-  if (is_playing)
-      requestaframe(loop);
+  if (is_playing) requestaframe(loop);
 }
 
 function start_loop() {
@@ -742,47 +827,47 @@ function stop_loop() {
 function key_down(e) {
   var key_id = e.keyCode || e.which;
 
-  if (key_id == 37) //left key
-  {
-      player.is_leftkey = true;
-      e.preventDefault();
-  } else if (key_id == 39) //right key
-  {
-      player.is_rightkey = true;
-      e.preventDefault();
-  } else if (key_id == 49 && player.Charaktergewählt == false) //right key
-  {
-      player.aktSpieler = 1;
-      player.Charaktergewählt = true;
-      e.preventDefault();
-  } else if (key_id == 50 && player.Charaktergewählt == false) //right key
-  {
-      player.aktSpieler = 2;
-      player.Charaktergewählt = true;
-      e.preventDefault();
-  } else if (key_id == 51 && player.Charaktergewählt == false) //right key
-  {
-      player.aktSpieler = 3;
-      player.Charaktergewählt = true;
-      e.preventDefault();
-  } else if (key_id == 52 && player.Charaktergewählt == false) //right key
-  {
-      player.aktSpieler = 4;
-      player.Charaktergewählt = true;
-      e.preventDefault();
+  if (key_id == 37) {
+    //left key
+    player.is_leftkey = true;
+    e.preventDefault();
+  } else if (key_id == 39) {
+    //right key
+    player.is_rightkey = true;
+    e.preventDefault();
+  } else if (key_id == 49 && player.Charaktergewählt == false) {
+    //right key
+    player.aktSpieler = 1;
+    player.Charaktergewählt = true;
+    e.preventDefault();
+  } else if (key_id == 50 && player.Charaktergewählt == false) {
+    //right key
+    player.aktSpieler = 2;
+    player.Charaktergewählt = true;
+    e.preventDefault();
+  } else if (key_id == 51 && player.Charaktergewählt == false) {
+    //right key
+    player.aktSpieler = 3;
+    player.Charaktergewählt = true;
+    e.preventDefault();
+  } else if (key_id == 52 && player.Charaktergewählt == false) {
+    //right key
+    player.aktSpieler = 4;
+    player.Charaktergewählt = true;
+    e.preventDefault();
   }
 }
 
 function key_up(e) {
   var key_id = e.keyCode || e.which;
 
-  if (key_id == 37) //left key
-  {
-      player.is_leftkey = false;
-      e.preventDefault();
-  } else if (key_id == 39) //right key
-  {
-      player.is_rightkey = false;
-      e.preventDefault();
+  if (key_id == 37) {
+    //left key
+    player.is_leftkey = false;
+    e.preventDefault();
+  } else if (key_id == 39) {
+    //right key
+    player.is_rightkey = false;
+    e.preventDefault();
   }
 }
